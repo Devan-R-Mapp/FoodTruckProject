0fde7c96628de31b5507c2d31ed57603eae88fc3
Devan Mapp <Devan.R.Mapp@gmail.com>
2022-03-27 09:47:19 -0600

    First progress

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,9 +1,37 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		FoodTruck FTON = new FoodTruck();
+		FoodTruck FTTW = new FoodTruck();
+		FoodTruck FTTH = new FoodTruck();
+		FoodTruck FTFO = new FoodTruck();
+		FoodTruck FTFI = new FoodTruck();
+		
+		
+		
+		System.out.println("Welcome to the food truck comparision app please enter up to 5 trucks.");
+		System.out.println("If you have less than 5 trucks, enter \"quit\" for the trucks name to move to the comparison.");
+		
+		for(int i = 0; i < 5; i++) {
+			String name = "";
+			String food = "";
+			double rating = 0.0;
+			
+			System.out.println("Please enter the name of the food truck: ");
+			name = sc.nextLine();
+			System.out.println("Please enter the type of food this truck serves: ");
+			food = sc.nextLine();
+			System.out.println("What is this trucks rating: ");
+			rating = sc.nextDouble();
+		}
+		
 
 	}
 
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,53 @@
 package com.skilldistillery.foodtruck.entities;
 
+
 public class FoodTruck {
+	private String name;
+	private String foodType;
+	private double rating; 
 	private static int nextTruckId;
+	public String getName() {
+		return name;
+	}
+	public FoodTruck(String name, String foodType, double rating) {
+		
+		
+	}
+	public FoodTruck() {
+		
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFoodType() {
+		return foodType;
+	}
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public static int getNextTruckId() {
+		return nextTruckId;
+	}
+	public static void setNextTruckId(int nextTruckId) {
+		FoodTruck.nextTruckId = nextTruckId;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FoodTruck: ");
+		builder.append(name);
+		builder.append(", foodType: ");
+		builder.append(foodType);
+		builder.append(", rating: ");
+		builder.append(rating);
+		return builder.toString();
+	}
+	
 }
